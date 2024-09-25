d16a17316976d792dab01863678f7c9a047c9c13
4rshi7 <arshit27202@gmail.com>
2024-09-25 11:20:33 +0530

    addition of navbar and footer and minor cosmetic changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,8 +3,8 @@ import { HiHome } from "react-icons/hi";
 
 export default function Header({name}) {
     return <>
-        <div className="min-h-[12vw] bg-gray-300 w-screen flex items-center">
-            <div className={"flex flex-col items-smart ml-[10%] p-3"}>
+        <div className="pt-20 min-h-[14vw] bg-gray-300 w-screen flex items-center">
+            <div className={"flex flex-col items-start ml-[10%] p-3"}>
                 <div id={"title"} className={"text-5xl font-semibold mb-2"}>{name}</div>
                 <Breadcrumb aria-label="Default breadcrumb example" className={"text-5xl"}>
                     <Breadcrumb.Item href="#" icon={HiHome}>
@@ -13,6 +13,6 @@ export default function Header({name}) {
                     <Breadcrumb.Item className="text-5xl">{name}</Breadcrumb.Item>
                 </Breadcrumb>
             </div>
-        </div>
+        </div>  
     </>
 }
