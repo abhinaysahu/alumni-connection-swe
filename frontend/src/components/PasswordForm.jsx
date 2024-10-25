d80700f8747c7880181891761e2c43d40e1c6bc6
abhinaysahu <abhin4ysahu@gmail.com>
2024-10-25 22:35:07 +0530

    Password Form updated

--- frontend/src/components/PasswordForm.jsx
+++ frontend/src/components/PasswordForm.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
+import { Button, Label } from "flowbite-react";
 
 export default function PasswordForm() {
   const [currentPassword, setCurrentPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
   const [isCurrentPasswordVisible, setCurrentPasswordVisible] = useState(false);
-  const [isConfirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+  const [isNewPasswordVisible, setNewPasswordVisible] = useState(false);
 
   const toggleCurrentPasswordVisibility = () => {
     setCurrentPasswordVisible(!isCurrentPasswordVisible);
   };
 
-  const toggleConfirmPasswordVisibility = () => {
-    setConfirmPasswordVisible(!isConfirmPasswordVisible);
+  const toggleNewPasswordVisibility = () => {
+    setNewPasswordVisible(!isNewPasswordVisible);
   };
 
   const handleSubmit = (e) => {
@@ -22,12 +23,8 @@ export default function PasswordForm() {
   return (
     <form onSubmit={handleSubmit} className="p-6">
       <div className="mb-4">
-        <label
-          htmlFor="currentPassword"
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
-          Current Password
-        </label>
+        <Label htmlFor="currentPassword" value="Current Password" />
+
         <div className="relative">
           <input
             type={isCurrentPasswordVisible ? "text" : "password"}
@@ -46,39 +43,28 @@ export default function PasswordForm() {
           </button>
         </div>
       </div>
-
       <div className="mb-6">
-        <label
-          htmlFor="confirmPassword"
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
-          Confirm Password
-        </label>
+        <Label htmlFor="newPassword" value="New Password" />
         <div className="relative">
           <input
-            type={isConfirmPasswordVisible ? "text" : "password"}
-            id="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
+            type={isNewPasswordVisible ? "text" : "password"}
+            id="newPassword"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
             required
           />
           <button
             type="button"
-            onClick={toggleConfirmPasswordVisibility}
+            onClick={toggleNewPasswordVisibility}
             className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
           >
-            {isConfirmPasswordVisible ? "Hide" : "Show"}
+            {isNewPasswordVisible ? "Hide" : "Show"}
           </button>
         </div>
       </div>
       <br></br>
-      <button
-        type="submit"
-        className="bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
-      >
-        Update Password
-      </button>
+      <Button type="submit">Update Password </Button>
     </form>
   );
 }
