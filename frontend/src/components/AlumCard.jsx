f90457d96f44a0a8e32ea6535a73681342a6dbec
4rshi7 <arshit27202@gmail.com>
2024-10-29 01:33:53 +0530

    alumni page added, job form revamp, changes made here are not final and are subject to modification

--- frontend/src/components/AlumCard.jsx
+++ frontend/src/components/AlumCard.jsx
@@ -1,27 +1,45 @@
 
-import {useState} from 'react';
+import { useState } from 'react';
 import { Button, Card } from "flowbite-react";
 import { Modal } from "flowbite-react";
-import axios from "axios";
+import axios from "axios"
+import { Link } from 'react-router-dom';
 
-export default function AlumCard(){
-return <>
-  <Card className="max-w-sm">
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {name}
-            </h5>
+export default function AlumCard({ name, batch, jobTitle, contact, company, email, linkedin, userId, photo }) {
+  return <>
+    <Card >
+      <div className="font-semibold text-lg text-center">{name}</div>
+      <div className="flex justify-around gap-0 ">
+        <div className='content-center'>
+          <img src='photo' alt="profile photo"></img>
+        </div>
+        {/* may be add userId to it to get profile  */}
+        <div>
+          <div>
             <div className="font-normal text-gray-700 dark:text-gray-400">
-                <table className={"border-separate border-spacing-4"}>
-                    <tr>
-                        <td className={"font-bold text-left"}>Current Position: </td>
-                        <td>{jobTitle}</td>
-                    </tr>
-                    <tr>
-                        <td className={"font-bold"}>Passout Year: </td>
-                        <td>{batch}</td>
-                    </tr>
-                </table>
+              <table className={"border-separate border-spacing-4"}>
+              <tr>
+                  <td className={"font-bold text-left"}>Company: </td>
+                  <td>{company}</td>
+                </tr>
+                <tr>
+                  <td className={"font-bold text-left"}>Current Position: </td>
+                  <td>{jobTitle}</td>
+                </tr>
+                <tr>
+                  <td className={"font-bold"}>Passout Year: </td>
+                  <td>{batch}</td>
+                </tr>
+              </table>
             </div>
-            </Card>
-</>
+          </div>
+        </div>
+        </div>
+        <div className=" flex  items-center justify-center w-full ">
+            <Link to={`/profile/${userId}`} >
+              <Button >Know more</Button>
+            </Link>
+          </div>
+    </Card>
+  </>
 }
