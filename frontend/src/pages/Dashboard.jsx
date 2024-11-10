5662713aa6df12325857b0cc8c25832924aa408d
4rshi7 <arshit27202@gmail.com>
2024-11-10 15:59:09 +0530

    dashboard changes

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import { IoSettingsOutline } from "react-icons/io5";
 import { HiTable } from "react-icons/hi";
 import { IoIosBriefcase } from "react-icons/io";
+import { FaUser } from "react-icons/fa";
 
 
 
@@ -14,15 +15,14 @@ import { IoIosBriefcase } from "react-icons/io";
 
 export default function Dashboard() {
   return (
-    <div className=" h-screen">
-    <div className="flex gap-2 h-full ">
+    <div className="flex gap-2 h-full flex-grow">
       {/* Sidebar */}
       <div className="bg-slate-50 flex-auto w-[10%] pt-10">
         <SideMenu />
       </div>
 
       {/* Main Content */}
-      <div className="bg-gray-200 flex-auto w-[80%] pt-10 mt-6 px-4 h-full">
+      <div className="bg-gray-200 flex-auto w-[80%] pt-10 mt-6 px-4 ">
         <div className="w-full pl-4 mb-2">
           {/* Page Header */}
           <div className="flex justify-between items-center mb-1  ">
@@ -36,26 +36,41 @@ export default function Dashboard() {
          
           {/* Cards Section */}
           <div className="flex justify-center mt-12 gap-16">
-              <button className="w-64 h-40 flex flex-col items-center justify-center  rounded-2xl bg-white">
-              <IoIosBriefcase className="h-8 w-8 text-indigo-500" />
-              <span className="text-center text-lg font mt-2">All Jobs</span>
-              </button>
-              <button className="w-64 h-40 flex flex-col items-center justify-center  rounded-2xl bg-white">
-              <UsersIcon className="h-8 w-8 text-indigo-500" />
-              <span className="text-center text-lg font mt-2">Alumni</span>
-              </button>
-              <button className="w-64 h-40 flex flex-col items-center justify-center  rounded-2xl bg-white">
-              <IoSettingsOutline className="h-8 w-8 text-indigo-500" />
-              <span className="text-center text-lg font mt-2">Profile Settings</span>
-              </button>
-              <button className="w-64 h-40 flex flex-col items-center justify-center  rounded-2xl bg-white">
-              <HiTable className="h-8 w-8 text-indigo-500" />
-              <span className="text-center text-lg font mt-2">Change Password</span>
-              </button>
+              <Link to="/jobs">
+              <Card className="w-64 h-40 flex flex-col items-center justify-center  rounded-2xl bg-white hover:bg-blue-400 hover:scale-105 transition-transform duration-300 hover:text-slate-50 text-blue-500 ">
+                <div className="flex justify-center items-center">
+              <IoIosBriefcase className="h-8 w-8 " />
+              </div>
+              <div className="text-center text-lg font mt-2">All Jobs</div>
+              </Card>
+              </Link>
+              <Link to = "/alumni">
+              <Card className="w-64 h-40 flex flex-col items-center justify-center  rounded-2xl bg-white  hover:bg-blue-400 hover:scale-105 transition-transform duration-300 hover:text-slate-50 text-blue-500 ">
+              <div className="flex justify-center items-center">
+              <UsersIcon className="h-8 w-8 " />
+              </div>
+              <div className="text-center text-lg font mt-2">Alumni</div>
+              </Card>
+              </Link>
+              <Link to="/myprofile">
+              <Card className="w-64 h-40 flex flex-col items-center justify-center  rounded-2xl bg-white  hover:bg-blue-400 hover:scale-105 transition-transform duration-300 hover:text-slate-50 text-blue-500 ">
+              <div className="flex justify-center items-center">
+              <FaUser className="h-8 w-8 " />
+              </div>
+              <div className="text-center text-lg font mt-2">My profile</div>
+              </Card>
+              </Link>
+              <Link to="/changepassword">
+              <Card className="w-64 h-40 flex flex-col items-center justify-center  rounded-2xl bg-white  hover:bg-blue-400 hover:scale-105 transition-transform duration-300 hover:text-slate-50 text-blue-500 ">
+              <div className="flex justify-center items-center">
+              <HiTable className="h-8 w-8 " />
+              </div>
+              <div className="text-center text-lg font mt-2">Change Password</div>
+              </Card>
+              </Link>
             </div>
 
         </div>
-      </div>
       </div>
       </div>
   );
